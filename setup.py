987517686708d9764e67e88e249f5b5762f9627d
Chris Lamb <chris@chris-lamb.co.uk>
2015-10-25 18:34:25 +0000

    Use dh-virtualenv

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='stravabot',
+    version='1.0.0',
+    license="BSD",
+
+    packages=(),
+    description="",
+
+    author='Chris Lamb',
+    author_email='chris@chris-lamb.co.uk',
+
+    scripts=(
+        'stravabot',
+    ),
+)
